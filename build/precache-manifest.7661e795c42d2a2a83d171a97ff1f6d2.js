af5f5fcbe4d9c87fe24bef56254cac2b85e178ce
Qian Wang <35624031+Persdre@users.noreply.github.com>
2020-02-02 12:13:04 +0800

    Add files via upload

---
+++ build/precache-manifest.7661e795c42d2a2a83d171a97ff1f6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a71b835c2689cd9ca24cd7e9e8d17ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96599a3f3856fae78c27",
+    "url": "/static/css/main.0ec40c22.chunk.css"
+  },
+  {
+    "revision": "f3651239f3303d937bbe",
+    "url": "/static/js/2.978d2689.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.978d2689.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96599a3f3856fae78c27",
+    "url": "/static/js/main.5456e96f.chunk.js"
+  },
+  {
+    "revision": "723b3cc9b8ec153a2e23",
+    "url": "/static/js/runtime-main.d4991002.js"
+  }
+]);
